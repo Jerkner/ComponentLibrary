12194c909f655757ccf15c66b2475760b3fb14f7
Jerkner <philip@jerkner.se>
2023-11-02 21:10:05 +0000

    Update Testimonials.jsx

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { TestimonialMobileComponent } from "../components/testimonials/TestimonialMobile"
 import { TestimonialDesktopComponent } from "../components/testimonials/TestimonialDesktop"
 
-export default function Testimonial() {
+export default function Testimonials() {
     return (
         <section className="testimonial-content content">
             <TestimonialMobileComponent />
