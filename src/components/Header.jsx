fabec54cbf8523754791605941291024f4edf325
Jerkner <philip@jerkner.se>
2023-11-02 21:16:17 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ export default function Header() {
     return (
         <header>
             <nav>
-                <NavLink to="/">Badges</NavLink>
-                <NavLink to="/banners">Banners</NavLink>
+                <NavLink to="">Badges</NavLink>
+                <NavLink to="banners">Banners</NavLink>
                 <NavLink to="cards">Cards</NavLink>
-                <NavLink to="/testimonials">Testimonials</NavLink>
+                <NavLink to="testimonials">Testimonials</NavLink>
                 <NavLink to="tooltips">Tooltips</NavLink>
                 <NavLink to="toasts">Toasts</NavLink>
             </nav>
